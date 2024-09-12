73a37d44e9b84ba0fb3cc27ffdb2f998d308bde3
DSamuelQ <samuelquiji@gmail.com>
2024-09-11 23:47:06 -0600

    Creación de modelos, se excluyó la llave primaria en cada uno de los constructores ya que son autoincrementales.

---
+++ Models/src/main/java/org/wass/models/Purchase/EstadoCompraModel.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.wass.models.Purchase;
+
+/**
+ *
+ * @author SamuelQ
+ */
+public class EstadoCompraModel {
+    private int idEstadoCompra;
+    private String nombreEstadoCompra;
+
+    public EstadoCompraModel(String nombreEstadoCompra) {
+        this.nombreEstadoCompra = nombreEstadoCompra;
+    }
+
+    public int getIdEstadoCompra() {
+        return idEstadoCompra;
+    }
+
+    public void setIdEstadoCompra(int idEstadoCompra) {
+        this.idEstadoCompra = idEstadoCompra;
+    }
+
+    public String getNombreEstadoCompra() {
+        return nombreEstadoCompra;
+    }
+
+    public void setNombreEstadoCompra(String nombreEstadoCompra) {
+        this.nombreEstadoCompra = nombreEstadoCompra;
+    }
+    
+    
+}
