5a7a29ed978f1d4c10fabb14863592e614914c94
DSamuelQ <samuelquiji@gmail.com>
2024-10-12 17:56:41 -0600

    No se subieron los controladores en el anterior

---
+++ Controllers/java/org/wass/controllers/purchase/EstadoCompraController.java
@@ -0,0 +1,33 @@
+package org.wass.controllers.purchase;
+
+import org.wass.models.purchase.EstadoCompraModel;
+import java.util.List;
+
+import org.wass.models.purchase.EstadoCompraDAO;
+
+/**
+ *
+ * @author SamuelQ
+ */
+
+public class EstadoCompraController {
+    private EstadoCompraDAO estadoCompraDao;
+
+    public EstadoCompraController(EstadoCompraDAO estadoCompraDao) {
+        this.estadoCompraDao = estadoCompraDao;
+    }
+
+    // Obtener una lista de los nombres de estados de compra
+    public List<EstadoCompraModel> obtenerEstadosCompra() {
+        return estadoCompraDao.obtenerEstadosCompra();
+    }
+
+    // Obtener un estado de compra por su ID
+    public EstadoCompraModel obtenerUnEstadoCompraPorId(int idEstadoCompra) {
+        return estadoCompraDao.obtenerUnEstadoCompraPorId(idEstadoCompra);
+    }
+    //Método para obtener un estado de compra por su nombre
+    public EstadoCompraModel obtenerEstadoCompraPorNombre(String nombreEstadoCompra) {
+        return estadoCompraDao.obtenerEstadoCompraPorNombre(nombreEstadoCompra);
+    }
+}
