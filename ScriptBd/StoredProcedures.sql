905a42c54494a0e368fc03b62bd394217a3b448d
alexb <alexbollen21@gmail.com>
2024-10-22 20:09:04 -0600

    Actualización sp venta

--- ScriptBd/StoredProcedures.sql
+++ ScriptBd/StoredProcedures.sql
@@ -12,36 +12,67 @@ CREATE PROCEDURE spVenta(
 	IN idTipoPago INT,
 	IN idCaja INT,
 	IN idSerie VARCHAR(25),
-	IN detalleVenta JSON
+	IN detalleVenta JSON,
+	OUT mensaje VARCHAR(100),
+	OUT estado INT
 )
-BEGIN
+VentaSP:BEGIN
 	DECLARE idVenta INT;
 	DECLARE i INT DEFAULT 0;
 	DECLARE n INT DEFAULT 0;
 	DECLARE idStock INT;
 	DECLARE cantidad INT;
 	
-	-- CREAR FACTURA
-	INSERT INTO factura (NoFactura, TotalFactura, FechaFactura, IdCliente, IdTipoPago, IdCaja, IdSerie)
-	VALUES (noFactura, totalFactura, fechaFactura, idCliente, idTipoPago, idCaja, idSerie);
+	-- MANEJO DE POSIBLES ERRORES
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+		GET DIAGNOSTICS CONDITION 1
+			mensaje = MESSAGE_TEXT;
+		ROLLBACK;
+		SET estado = 0;
+	END;
 	
-	SET idVenta = LAST_INSERT_ID();
-	
-	SET n = JSON_LENGTH(detalleventa);
-	
-	-- CREAR REGISTROS DE DETALLES DE VENTA Y DESCONTAR STOCKS
-	WHILE i < n DO
-		SET idStock = JSON_UNQUOTE(JSON_EXTRACT(detalleVenta, CONCAT('$[',i,'].idStock')));
-		SET cantidad = JSON_UNQUOTE(JSON_EXTRACT(detalleventa	, CONCAT('$[',i,'].cantidad')));
+	START TRANSACTION;
+		SET n = JSON_LENGTH(detalleVenta);
 		
-		INSERT INTO detalleventa (IdStock, IdFactura, Cantidad)
-		VALUES (idStock, idVenta, cantidad);
-		
-		UPDATE Stock SET CantidadDisponible = Stock.CantidadDisponible - cantidad
-		WHERE Stock.IdStock = idStock;
-		
-		SET i = i + 1;
-	END WHILE;
+		-- VERIFICACIÓN DE PRODUCTOS EN LA VENTA
+		IF detalleVenta IS NULL OR n = 0 THEN
+			SET mensaje = 'No se asignaron productos a la venta';
+			SET estado = 0;
+			ROLLBACK;
+			LEAVE VentaSP;
+		END IF;
 	
+		-- CREAR FACTURA
+		INSERT INTO Factura (NoFactura, TotalFactura, FechaFactura, IdCliente, IdTipoPago, IdCaja, IdSerie)
+		VALUES (noFactura, totalFactura, fechaFactura, idCliente, idTipoPago, idCaja, idSerie);
+		
+		SET idVenta = LAST_INSERT_ID();
+		
+		-- CREAR REGISTROS DE DETALLES DE VENTA Y DESCONTAR STOCKS
+		WHILE i < n DO
+			SET idStock = JSON_UNQUOTE(JSON_EXTRACT(detalleVenta, CONCAT('$[',i,'].idStock')));
+			SET cantidad = JSON_UNQUOTE(JSON_EXTRACT(detalleVenta	, CONCAT('$[',i,'].cantidad')));
+			
+			-- VERIFICAR EXISTENCIAS SUFICIENTES PARA EL DESCUENTO
+			IF (SELECT CantidadDisponible FROM Stock WHERE IdStock = idStock LIMIT 1) < cantidad THEN
+				SET mensaje = CONCAT('Existencias insuficientes en stock con id: ', idStock);
+				SET estado = 0;
+				ROLLBACK; 
+				LEAVE VentaSP;  
+			END IF;
+			
+			INSERT INTO DetalleVenta (IdStock, IdFactura, Cantidad)
+			VALUES (idStock, idVenta, cantidad);
+			
+			UPDATE Stock SET CantidadDisponible = Stock.CantidadDisponible - cantidad
+			WHERE Stock.IdStock = idStock;
+			
+			SET i = i + 1;
+		END WHILE;
+		SET mensaje = 'Venta realizada correctamente';
+		SET estado = 1;
+		COMMIT ;
+
 END$$
 DELIMITER ;
